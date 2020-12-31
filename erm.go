d615bc32e31e61ba07450fcd426e75777cd4093c
8i8 <iainardernhill@gmail.com>
2020-12-31 15:59:13 +0100

    tests added

--- erm.go
+++ erm.go
@@ -10,13 +10,19 @@ type ErrMsg interface {
 	Read() string
 }
 
+type ErrMsgWriter interface {
+	Error() string
+	Read() string
+	Write(string) errmsg
+}
+
 func (e errmsg) Error() string           { return e.err }
 func (e errmsg) Read() string            { return e.msg }
-func (e errmsg) Write(msg string) errmsg { e.msg = msg; return e }
+func (e *errmsg) Write(msg string) errmsg { e.msg = msg; return *e }
 
-// NewErrMsg returns and error of types err that returns a message when
-// calle.
-func NewErrMsg(err interface{}, m string) errmsg {
+// New returns an error of types errmsg that conforms to the ErrMsg
+// interface, which adds the Read{} functionality for passing messages.
+func New(err interface{}, m string) errmsg {
 	if e, ok := err.(error); ok {
 		return errmsg{err: e.Error(), msg: m}
 	}
--- erm_test.go
+++ erm_test.go
@@ -3,11 +3,12 @@ package erm
 import (
 	"errors"
 	"fmt"
+	"strings"
 	"testing"
 )
 
 var errType = errors.New("main error type")
-var myErrMsg = NewErrMsg(errType, "Ops, an error occured!")
+var myErrMsg = New(errType, "Ops, an error occured!")
 
 func errGen() error {
 	return myErrMsg.Write("This is the key message!")
@@ -22,12 +23,12 @@ func TestErm(t *testing.T) {
 	const fname = "TestErm"
 	err := errWrapper()
 
-	//if !errors.Is(err, myErrMsg) {
-	if !errors.Is(err, err) {
-		t.Errorf("%s: want %T got %T", fname, myErrMsg, errors.Unwrap(err))
+	if _, ok := errors.Unwrap(err).(ErrMsg); !ok {
+		t.Errorf("%s: want %T got %T", fname, myErrMsg, err)
 	}
-	errCore, ok := errors.Unwrap(err).(ErrMsg)
-	if !ok {
-		t.Errorf("%s: want %T got %T", fname, errType, errCore)
+	str := fmt.Sprint(errors.Unwrap(err).Error())
+	str2 := fmt.Sprint(errType.Error())
+	if strings.Compare(str, str2) != 0 {
+		t.Errorf("%s: want %q got %q", fname, str2, str)
 	}
 }
